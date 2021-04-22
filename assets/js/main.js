d273e7c73995d3114a3d814b2cedccf486e6f886
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-04-22 14:34:30 +0100

    Fina Commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// User Name
+var userName = prompt("What's your name?")
+console.log(userName)
+// User Surname
+var userSurname = prompt("What's your surname?")
+
+// User Favourite Colour
+var favColour = prompt("What's your favourite colour?")
+
+// Resulted Message
+var result = document.getElementById('message')
+message.innerHTML = userName + userSurname + favColour + "21"
